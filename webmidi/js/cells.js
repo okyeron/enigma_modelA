7a1d2a112b1d18f4439cf39ecef7c2fb41c4c4b3
okyeron <nevetsokyeron@NyanNyan-6.local>
2019-01-19 15:38:24 -0600

    add webmidi demo

---
+++ webmidi/js/cells.js
@@ -0,0 +1,225 @@
+var theUniverse = null;
+
+var numRows = 8,
+	numCols = 16;
+
+window.addEventListener('keydown', function() { 
+} );
+
+window.addEventListener('load', function() {
+	theUniverse = document.getElementById("universe");
+	
+	var t = document.createElement("div");
+	t.setAttribute("name", "grid");
+	
+	var z = 0;
+	for (var row = 0; row < numRows; row++) {
+		var newRow = document.createElement("div");
+		newRow.className = "row";
+		newRow.row = row;
+		//console.log(newRow);
+		t.appendChild(newRow);
+		for (var col = 0; col < numCols; col++) {
+			var newCell = document.createElement("div");
+			newRow.appendChild(newCell);
+			newCell.row = row;
+			newCell.col = col;
+			newCell.onclick = flipHandler;
+			newCell.setAttribute("id", z);
+			newCell.className = "cell";
+			newCell.innerHTML=z; 
+			newRow.appendChild(newCell);
+			z = z+1;
+		}
+	}
+	document.body.appendChild(t);
+} );
+    
+    
+window.addEventListener("DOMContentLoaded", function(event) {
+  if (navigator.requestMIDIAccess) {
+	  console.log('This browser supports WebMIDI!');
+  } else {
+	  console.log('WebMIDI is not supported in this browser.');
+	  document.querySelector("#midi").style.display = "none";
+	  document.querySelector("#nomidi").style.display = "block";
+  }
+	navigator.requestMIDIAccess({sysex: true}).then( onMIDIInit, onMIDIFail );
+
+});
+
+var selectMIDIIn = null;
+var selectMIDIOut = null;
+var midiAccess = null; // global MIDIAccess object
+var midiIn = null;
+var midiOut = null;
+var enigmamidi = false;
+
+
+function changeMIDIIn( ev ) {
+  if (midiIn)
+    midiIn.onmidimessage = null;
+  var selectedID = selectMIDIIn[selectMIDIIn.selectedIndex].value;
+
+  for (var input of midiAccess.inputs.values()) {
+    if (selectedID == input.id)
+      midiIn = input;
+  }
+  midiIn.onmidimessage = midiProcess;
+}
+
+function changeMIDIOut( ev ) {
+  var selectedID = selectMIDIOut[selectMIDIOut.selectedIndex].value;
+
+  for (var output of midiAccess.outputs.values()) {
+    if (selectedID == output.id) {
+      midiOut = output;
+      console.log('changed to output ' + output.name);
+	}
+  }
+}
+
+
+function onMIDIInit(midi) {
+	midiAccess = midi;
+
+  //var inputs = midiAccess.inputs;
+  //var outputs = midiAccess.outputs;
+  
+  selectMIDIIn=document.getElementById("midiIn");
+  selectMIDIOut=document.getElementById("midiOut");
+  console.log('WebMIDI init');
+
+  for (var entry of midiAccess.inputs) {
+  	var item = entry[1];
+  	console.log( "Input port [type:'" + item.type + "'] id:'" + item.id + "' manufacturer:'" + item.manufacturer + "' name:'" + item.name + "' version:'" + item.version + "'" );
+  }
+
+  // clear the MIDI input select
+  selectMIDIIn.options.length = 0;
+  for (var input of midiAccess.inputs.values()) {
+    if ((input.name.toString().indexOf("enigmamidi") != -1)) {
+      enigmamidi = true;
+      selectMIDIIn.add(new Option(input.name,input.id,true,true));
+      midiIn=input;
+	  midiIn.onmidimessage = midiProcess;
+
+    }else{
+  		selectMIDIIn.add(new Option(input.name,input.id,false,false));
+  	}
+  }
+  selectMIDIIn.onchange = changeMIDIIn;
+
+  // clear the MIDI output select
+  selectMIDIOut.options.length = 0;
+  for (var output of midiAccess.outputs.values()) {
+    if ((output.name.toString().indexOf("enigmamidi") != -1)) {
+      selectMIDIOut.add(new Option(output.name,output.id,true,true));
+      midiOut=output;
+    }else{
+	    selectMIDIOut.add(new Option(output.name,output.id,false,false));
+	}
+  }
+  selectMIDIOut.onchange = changeMIDIOut;
+
+
+}
+
+function onMIDIFail() {
+  console.log('Could not access your MIDI devices.');
+}
+
+function onMIDISuccess(midiAccess) {
+	for (var input of midiAccess.inputs.values()) {
+		input.onmidimessage = midiProcess;
+	}
+}
+
+function midiProcess(ev) {
+	//console.log(ev);
+
+	var cmd = ev.data[0] >> 4;
+	var channel = ev.data[0] & 0xf;
+	var noteNumber = ev.data[1];
+	var velocity = ev.data[2];
+
+	if(cmd>14){ //handle sysex
+
+		if(ev.data[5]===0 && ev.data[6]===1 && ev.data[7]===97){
+			var id = ev.data[10];
+			//product = products[id]; //turn product ID into a name
+			clog('product: (id '+id+')');
+		}
+    
+	} else {
+
+	if ( cmd==8 || ((cmd==9)&&(velocity==0)) ) { // with MIDI, note on with velocity zero is the same as note off
+	// note off
+		noteOff( noteNumber );
+		console.log('noteOff: '+ noteNumber);
+	} else if (cmd == 9) {
+	// note on
+		noteOn( noteNumber, velocity);
+		console.log('noteOn: '+ noteNumber);
+	} else if (cmd == 11) {
+		//controller( noteNumber, velocity/127.0);
+		console.log('controller: '+ noteNumber);
+	} else if (cmd == 14) {
+		// pitch wheel
+		//pitchWheel( ((velocity * 128.0 + noteNumber)-8192)/8192.0 );
+	}
+	}
+}
+
+function $(id) {
+	return document.getElementById(id);
+}
+
+function noteOn( note, velocity ) {
+	//console.log(' note on '+note+' '+velocity);
+	$('notenum').innerHTML = note;
+	$('noteval').innerHTML = velocity;
+	$(note).className = "cell on";
+	
+}
+
+function noteOff( note ) {
+	//console.log('note off '+note);
+	$('notenum').innerHTML = note;
+	$('noteval').innerHTML = 0;
+	$(note).className = "cell";
+	
+}
+// 'value' is normalized to 0..1.
+function controller( number, value ) {
+	//console.log('cc '+number+' '+value);
+	$('ccnum').innerHTML = number;
+	$('ccval').innerHTML = value;
+	  //send back to controller to update LEDs
+	  //ring(number,value);
+}
+
+var currentPitchWheel = 0.0;
+// 'value' is normalized to [-1,1]
+function pitchWheel( value ) {
+	var i;
+	currentPitchWheel = value;
+	console.log('pitchwheel '+value);
+}
+
+function flipHandler(e) {
+	//console.log(e.target.id);
+	flip( e.target.id );
+}
+
+function flip(note) {
+	if ($(note).className == "cell"){  // dead
+		$(note).className = "cell on";
+		midiOut.send( [0x90, note, 0x7f] );
+	}else{
+		$(note).className = "cell";
+		midiOut.send( [0x80, note, 0x40] );
+	}
+	//var key = elem.row*16 + elem.col;
+	//midiOut.send( [0x90, elem, elem.classList.contains("on") ? (0x80) : 0x00]);
+}
